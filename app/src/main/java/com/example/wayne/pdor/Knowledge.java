0b833b4230d2314ccd6c3a90b56285d59cf59423
TimeFur <djs860403@gmail.com>
2018-02-25 20:59:11 +0800

    Webview implement
    1. Linking the Webview object to webview id
    2. Using webviewSetting to enable java scripts
    3. Setting the permission for INTERNET

--- app/src/main/java/com/example/wayne/pdor/Knowledge.java
+++ app/src/main/java/com/example/wayne/pdor/Knowledge.java
@@ -2,12 +2,23 @@ package com.example.wayne.pdor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Knowledge extends AppCompatActivity {
 
+    String _url = "https://www.google.com.tw";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knowledge);
+
+        //setting
+        WebView web_view = (WebView)findViewById(R.id.webView1);
+        WebSettings web_view_setting = web_view.getSettings();
+        web_view_setting.setJavaScriptEnabled(true);
+        web_view.loadUrl(_url);
     }
 }
