ac06d7c2c338bae2ede69f6029c3662a3cc21c16
TimeFur <djs860403@gmail.com>
2018-02-26 20:59:49 +0800

    Put the parameter for differnt fragment

--- app/src/main/java/com/example/wayne/pdor/Knowledge.java
+++ app/src/main/java/com/example/wayne/pdor/Knowledge.java
@@ -13,11 +13,12 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class Knowledge extends AppCompatActivity {
 
     String _url = "https://www.google.com.tw";
-
+    final int Fragment_num = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +27,7 @@ public class Knowledge extends AppCompatActivity {
         ViewPager view_pager = (ViewPager) findViewById(R.id.viewPager);
         PagerAdapter pager_adpt = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         view_pager.setAdapter(pager_adpt);
+
         //setting
 //        WebView web_view = (WebView)findViewById(R.id.webView1);
 //        WebSettings web_view_setting = web_view.getSettings();
@@ -41,20 +43,26 @@ public class Knowledge extends AppCompatActivity {
         @Override
         public android.support.v4.app.Fragment getItem(int position)
         {
-            return new ScreenSlidePageFragment();
+            ScreenSlidePageFragment fragment = new ScreenSlidePageFragment();
+            fragment.get_postion(position);
+            return fragment;
         }
         @Override
         public int getCount() {
-            return 5;
+            return Fragment_num;
         }
     }
 
-    static public class ScreenSlidePageFragment extends  android.support.v4.app.Fragment{
+    static public class ScreenSlidePageFragment extends android.support.v4.app.Fragment{
+        int position;
+        public void get_postion(int pos){
+            this.position = pos;
+        }
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            ViewGroup rootView = (ViewGroup) inflater.inflate(
-                    R.layout.screen_slide_page, container, false);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.screen_slide_page, container, false);
+            TextView tv = rootView.findViewById(R.id.screen_slide_tv1);
+            tv.setText("Hi" + this.position);
 
             return rootView;
         }
