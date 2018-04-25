63a7ac7fb0a8f3aeedc89ce5e2f6a4c9edb3ead7
TimeFur <djs860403@gmail.com>
2018-04-26 00:08:14 +0800

    Prune the Floating Setting

--- app/src/main/java/com/example/wayne/pdor/Floating_window.java
+++ app/src/main/java/com/example/wayne/pdor/Floating_window.java
@@ -21,59 +21,45 @@ import android.widget.ImageView;
 
 public class Floating_window extends Service{
 
-    Handler handler;
-
     public void onCreate(){
         super.onCreate();
-
-//        handler = new Handler();
-//        handler.post(floating_thread);
-
-        //Bind Setting
-//        win_manage.addView(win_view, wmParams);
         create_floating();
-//        floating2();
     }
 
     void create_floating(){
 
         LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        //Declare the WinManager, Layout & View ------Step1
         WindowManager win_manage = null;
-        WindowManager.LayoutParams win_param = null;
-
-        win_manage = (WindowManager) getSystemService(WINDOW_SERVICE);
+        WindowManager.LayoutParams win_param = new WindowManager.LayoutParams();;
         View win_view;
+
+        //Setting the WindowManager  ------Step2.1
+        win_manage = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        //Setting the WindowManager  ------Step2.2
         win_view = layoutInflater.inflate(R.layout.floating_layout, null);
         ImageView img = win_view.findViewById(R.id.floating_view);
         img.setImageResource(R.drawable.ic_ballon);
 
-        //Setting window parameter
-        WindowManager.LayoutParams wmParams = new WindowManager.LayoutParams();
-
+        //Setting the Layout parameter -----Step2.3
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) // >= API26
-            wmParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            win_param.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
         else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) // >= API19
-            wmParams.type = WindowManager.LayoutParams.TYPE_TOAST;
+            win_param.type = WindowManager.LayoutParams.TYPE_TOAST;
         else
-            wmParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
-        wmParams.format = PixelFormat.TRANSLUCENT;
-        wmParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
+            win_param.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        win_param.format = PixelFormat.TRANSLUCENT;
+        win_param.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
-        wmParams.gravity = Gravity.LEFT | Gravity.TOP;
-        wmParams.width = 100;
-        wmParams.height = 100;
+        win_param.gravity = Gravity.LEFT | Gravity.TOP;
+        win_param.width = 100;
+        win_param.height = 100;
 
-        //Bind Setting
-        win_manage.addView(win_view, wmParams);
+        //Bind Setting from winView & winParam -----Step3
+        win_manage.addView(win_view, win_param);
     }
 
-    Runnable floating_thread = new Runnable() {
-        @Override
-        public void run() {
-            create_floating();
-        }
-    };
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -83,26 +69,6 @@ public class Floating_window extends Service{
         Floating_window getBinder(){
             return Floating_window.this;
         }
-    }
-
-    void floating2()
-    {
-        WindowManager windowManager2 = (WindowManager)getSystemService(WINDOW_SERVICE);
-        LayoutInflater layoutInflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view=layoutInflater.inflate(R.layout.floating_layout, null);
-        ImageView img = view.findViewById(R.id.floating_view);
-        img.setImageResource(R.drawable.ic_ballon);
-        WindowManager.LayoutParams params=new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-
-        params.gravity=Gravity.CENTER|Gravity.CENTER;
-        params.x=0;
-        params.y=0;
-        windowManager2.addView(view, params);
     }
 
 }
