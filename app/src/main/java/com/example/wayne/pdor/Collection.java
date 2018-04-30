2183e120f57819749bf256555087c90b0a4b3cf9
TimeFur <djs860403@gmail.com>
2018-05-01 00:02:30 +0800

    Using the AsyncTask to play
    1. The MediaPlayer is used to set resource & play music.
    2. The AsyncTask is using the PostTask to start music at first time.

--- app/src/main/java/com/example/wayne/pdor/Collection.java
+++ app/src/main/java/com/example/wayne/pdor/Collection.java
@@ -8,13 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class Collection extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
     private Button play_btn;
-    private Button stop_btn;
+    private boolean play_status = false;
+    private boolean initial_status = true;
+    private String url = "https://www.ssaurel.com/tmp/mymusic.mp3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +28,28 @@ public class Collection extends AppCompatActivity {
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
         play_btn = findViewById(R.id.collectio_play_btn);
-        stop_btn = findViewById(R.id.collectio_stop_btn);
 
         play_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-            }
-        });
-        stop_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
+                if (mediaPlayer.isPlaying() == false)
+                {
+                    //Start to play
+                    if (initial_status == true) {
+                        new Stream_audio().execute(url);
+                        initial_status = false;
+                    }
+                    else
+                    {
+                        if (mediaPlayer.isPlaying() == false)
+                            mediaPlayer.start();
+                    }
+                }
+                else
+                {
+                    //Stop
+                    mediaPlayer.pause();
+                }
             }
         });
     }
@@ -44,14 +57,38 @@ public class Collection extends AppCompatActivity {
     /*========================================
                    Stream data class
      ========================================*/
-    class Stream_audio extends AsyncTask<URL, Integer, Long>
+    class Stream_audio extends AsyncTask<String, Integer, Long>
     {
         @Override
-        protected Long doInBackground(URL... urls) {
+        protected Long doInBackground(String... urls) {
+            try
+            {
+                mediaPlayer.setDataSource(urls[0]);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset(); //it should set the resource again & call prepare
+                        initial_status = true;
+                    }
+                });
 
+                mediaPlayer.prepare(); //player for playback (synchronously)
+
+            }catch (Exception e){
+
+            }
 
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+
+            mediaPlayer.start();
+
         }
     }
 }
